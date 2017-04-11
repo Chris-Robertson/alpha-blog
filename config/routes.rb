ad54eb33739282d5bc1997fe194065a87c56e85c
Chris Robertson <chrisxrobertson@gmail.com>
2017-04-11 19:33:50 +1000

    Adding article submission form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get root 'pages#home'
   get '/about', to: 'pages#about'
-  get '/articles', to: 'pages#articles'
+
+  resources :articles
 
 end
