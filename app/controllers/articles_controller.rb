da51af3eab1e270bf4c1828c21914a46beaddf37
Chris Robertson <chrisxrobertson@gmail.com>
2017-04-12 11:44:03 +1000

    Drying out some code.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    set_article
   end
 
   def create
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+    set_article
     if @article.update(article_params)
       flash[:notice] = "Article succsessfully edited"
       redirect_to article_path(@article)
@@ -33,17 +33,21 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    set_article
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    set_article
     @article.delete
     flash[:notice] = "Article succsessfully deleted"
     redirect_to articles_path
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
