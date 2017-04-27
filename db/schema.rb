eb1a9be39e8d787240e7b12c9af436e4b62fd743
Chris Robertson <chrisxrobertson@gmail.com>
2017-04-27 17:13:36 +1000

    Created user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424054958) do
+ActiveRecord::Schema.define(version: 20170427063909) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170424054958) do
     t.datetime "updated_at", null: false
     t.index ["photo_id"], name: "index_likes_on_photo_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
