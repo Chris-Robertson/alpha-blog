bfeb2b2fc4de7998a8b698f1ca46ca7f1d7c617f
Chris Robertson <chrisxrobertson@gmail.com>
2017-04-27 21:57:38 +1000

    Added user article association. Added debug view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427063909) do
+ActiveRecord::Schema.define(version: 20170427084157) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "likes", force: :cascade do |t|
