a17e9684113393bdc313ded4932b0b1a145093a0
Chris Robertson <chrisxrobertson@gmail.com>
2017-05-07 17:06:31 +1000

    Add edit user funcionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get root 'pages#home'
   get '/about', to: 'pages#about'
 
   resources :articles
-
   resources :users, except: [:new]
+
   get 'signup', to: 'users#new'
+
 end
